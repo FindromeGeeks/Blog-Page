077be0d0079ead98756afdcbb3137bce57843d94
Rudra <patelrudra.3000@gmail.com>
2023-12-29 12:21:24 +0530

    added FontAwesome Icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import styles from './navbar.module.css'
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookSquare, faSquareInstagram, faSquareThreads, faLinkedin, faSquareXTwitter } from '@fortawesome/free-brands-svg-icons'
 
 export default function Navbar() {
   return (
     <div className={styles.container}>
-      Navbar
+      <div className={styles.social}>
+        <FontAwesomeIcon icon={faFacebookSquare} width={24} height={24}/>
+        <FontAwesomeIcon icon={faSquareInstagram} width={24} height={24}/>
+        <FontAwesomeIcon icon={faSquareThreads} width={24} height={24}/>
+        <FontAwesomeIcon icon={faLinkedin} width={24} height={24}/>
+        <FontAwesomeIcon icon={faSquareXTwitter} width={24} height={24}/>
+      </div>
+      <div className={styles.logo}></div>
+      <div className={styles.links}></div>
+
     </div>
   )
 }
